fb43dbce87de1a523f84887a5f0643d2a3163e95
nguyenlam455 <livefortoday01455@gmail.com>
2019-02-02 14:29:28 +0700

    Initial Commit

---
+++ MyInsta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyInsta
+//
+//  Created by Nguyen Lam on 2/2/19.
+//  Copyright © 2019 Nguyen Lam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
